664c6523fe934b8809778d7c183066a1dde67a5f
Maria <mariaga@ac.sce.ac.il>
2020-09-02 20:49:31 +0300

    new updates

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -1,106 +1,247 @@
-import React from 'react'
-import './Room.css'
+import React, { useState, useEffect } from 'react';
+import './Room.css';
 import axios from 'axios';
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
-import countDownTimer from '../countDownTimer/countDownTimer'
-
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import countDownTimer from '../countDownTimer/countDownTimer';
 
 const Room = () => {
+  //the set of all questions
+  const [questionsSet, setQuestionsSet] = useState([]);
+  //question number
+  const [questionNumber, setQuestionNumber] = useState(1);
+  // qurrent question
+  const [currentQuestion, setCurrentQuestion] = useState({});
+  // answers array
+  const [possibleAnswers, setPossibleAnswers] = useState([]);
+  //score
+  const [score, setScore] = useState(0);
+  //hearts
+  const [hearts, setHearts] = useState(3);
 
+  useEffect(() => {
     axios
-    .get(
-      'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple'
-    )
-    .then((response) => {
-      console.log(response.data.results[0]);
-    });
+      .get(
+        'https://opentdb.com/api.php?amount=15&category=18&difficulty=medium&type=multiple'
+      )
+      .then((response) => {
+        setQuestionsSet(response.data.results);
+        let tmp = response.data.results[0];
+        setCurrentQuestion(tmp);
+        let arr = tmp.incorrect_answers;
+        arr.push(tmp.correct_answer);
+        shuffleArray(arr);
+        setPossibleAnswers(arr);
+      });
+  }, []);
 
-    return (
-        <div className='room'>
+  const getQuetion = () => {
+    const a = document.querySelector('#a');
+    a.style.background = 'rgb(231, 231, 231)';
+    const b = document.querySelector('#b');
+    b.style.background = 'rgb(231, 231, 231)';
+    const c = document.querySelector('#c');
+    c.style.background = 'rgb(231, 231, 231)';
+    const d = document.querySelector('#d');
+    d.style.background = 'rgb(231, 231, 231)';
+    setCurrentQuestion(questionsSet[questionNumber]);
+    let arr = questionsSet[questionNumber].incorrect_answers;
+    arr.push(questionsSet[questionNumber].correct_answer);
+    shuffleArray(arr);
+    setPossibleAnswers(arr);
+  };
 
-            <div className='room-container'>
+  const checkAnswer = (ans, section) => {
+    const answer = document.querySelector('#' + section);
 
-                <div className='room-header'>
-                    <h1>Trivia Game</h1>
-                    <div className='question-and-timer'>
-                        <div className='question-counter'>
-                            <p>Question</p>
-                            <p>1</p>
-                        </div>
-                        <div className='timer'>
-                            <CountdownCircleTimer
-                                isPlaying
-                                duration={30}
-                                // colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-                                size={90}
-                                // onComplete={() => [true, 1000]}
-                                colors={[["#FFFFFF", 0.33]]}
-                                >
-                                {countDownTimer}
-                            </CountdownCircleTimer>
-                        </div>
-                        <div className='user-lifes center'>
-                            <i class="fa fa-heart" aria-hidden="true"></i>
-                            <i class="fa fa-heart" aria-hidden="true"></i>
-                            <i class="fa fa-heart" aria-hidden="true"></i>
-                        </div>
-                    </div>
-                </div>
+    // check if the answer is right
+    if (ans === currentQuestion.correct_answer) {
+      setScore(score + 5);
+      playAudio('https://www.youtube.com/watch?v=0ZQ-Lk--ILE');
+      answer.style.background = 'green';
+    } else {
+      answer.style.background = 'red';
+      let tmp;
+      for (let i = 0; i < possibleAnswers.length; i++) {
+        if (currentQuestion.correct_answer === possibleAnswers[i]) {
+          tmp = i;
+          break;
+        }
+      }
+      console.log(tmp);
+      switch (tmp) {
+        case 0:
+          const a = document.querySelector('#a');
+          a.style.background = 'green';
+          break;
+        case 1:
+          const b = document.querySelector('#b');
+          b.style.background = 'green';
+          break;
+        case 2:
+          const c = document.querySelector('#c');
+          c.style.background = 'green';
+          break;
+        case 3:
+          const d = document.querySelector('#d');
+          d.style.background = 'green';
+          break;
+        default:
+          break;
+      }
+      if (hearts > 0) setHearts(hearts - 1);
+      else endGame();
+    }
 
-                <div className='room-question center'>
+    setTimeout(() => {
+      //check if the questions ended
+      if (questionNumber < 15) {
+        setQuestionNumber(questionNumber + 1);
+        getQuetion();
+      } else endGame();
+    }, 1000);
+  };
 
-                    <p >
-                        Lorem ipsum dolor, sit amet consectetur 
-                        adipisicing elit. Sunt, quasi recusandae! 
-                        Dolor modi culpa nobis amet aliquid vitae 
-                        rerum facilis at nesciunt, ut consequatur 
-                        dolore quo incidunt obcaecati labore ipsam.
-                    </p>
+  const timeEnded = () => {
+    if (hearts > 0) {
+      setHearts(hearts - 1);
+      if (questionNumber < 15) {
+        setQuestionNumber(questionNumber + 1);
+        getQuetion();
+      } else endGame();
+    } else endGame();
+  };
 
-                </div>
+  const endGame = () => {
+    console.log('endGame');
+  };
 
-                <div className='room-ans'>
-                    <div className='row'>
-                        <div className='ans-box center'>
-                            <div className='number-of-ans'>a</div>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quo.
-                        </div>
-                        <div className='ans-box center'>
-                            <div className='number-of-ans'>b</div>
+  function playAudio(url) {
+    new Audio(url).play();
+  }
 
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quo.
-                        </div>                    
-                    </div>
-             
-                    <div className='row'>
-                        <div className='ans-box center'>
-                            <div className='number-of-ans'>c</div>
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
 
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quo.
-                        </div>
-                        <div className='ans-box center'>
-                            <div className='number-of-ans'>d</div>
+  return (
+    <div className='room'>
+      <div className='room-container'>
+        <div className='room-header'>
+          <h1>Trivia Game</h1>
+          <div className='question-and-timer'>
+            <div className='question-counter'>
+              <p>Question</p>
+              <p>{questionNumber}</p>
+            </div>
+            <div className='timer'>
+              <CountdownCircleTimer
+                isPlaying
+                duration={30}
+                key={questionNumber}
+                // colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+                size={90}
+                onComplete={() => timeEnded()}
+                colors={[['#FFFFFF', 0.33]]}
+              >
+                {countDownTimer}
+              </CountdownCircleTimer>
+            </div>
+            <div className='user-lifes center'>
+              {hearts <= 2 ? (
+                <i class='fas fa-heart-broken'></i>
+              ) : (
+                <i class='fa fa-heart' aria-hidden='true'></i>
+              )}
+              {hearts <= 1 ? (
+                <i class='fas fa-heart-broken'></i>
+              ) : (
+                <i class='fa fa-heart' aria-hidden='true'></i>
+              )}
+              {hearts === 0 ? (
+                <i class='fas fa-heart-broken'></i>
+              ) : (
+                <i class='fa fa-heart' aria-hidden='true'></i>
+              )}
+            </div>
+          </div>
+        </div>
 
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quo.
-                        </div> 
-                    </div>
-                </div>
-                {/* <div className='exit-game-btn-div center'>
+        <div className='room-question center'>
+          <p dangerouslySetInnerHTML={{ __html: currentQuestion.question }}></p>
+        </div>
+
+        <div className='room-ans'>
+          <div className='row'>
+            <div
+              className='ans-box center'
+              id='a'
+              onClick={() => {
+                checkAnswer(possibleAnswers[0], 'a');
+              }}
+            >
+              <div className='number-of-ans'>a</div>
+              <label
+                dangerouslySetInnerHTML={{ __html: possibleAnswers[0] }}
+              ></label>
+            </div>
+            <div
+              className='ans-box center'
+              id='b'
+              onClick={() => {
+                checkAnswer(possibleAnswers[1], 'b');
+              }}
+            >
+              <div className='number-of-ans'>b</div>
+              <label
+                dangerouslySetInnerHTML={{ __html: possibleAnswers[1] }}
+              ></label>
+            </div>
+          </div>
+          <div className='row'>
+            <div
+              className='ans-box center'
+              id='c'
+              onClick={() => {
+                checkAnswer(possibleAnswers[2], 'c');
+              }}
+            >
+              <div className='number-of-ans'>c</div>
+              <label
+                dangerouslySetInnerHTML={{ __html: possibleAnswers[2] }}
+              ></label>
+            </div>
+            <div
+              className='ans-box center'
+              id='d'
+              onClick={() => {
+                checkAnswer(possibleAnswers[3], 'd');
+              }}
+            >
+              <div className='number-of-ans'>d</div>
+              <label
+                dangerouslySetInnerHTML={{ __html: possibleAnswers[3] }}
+              ></label>
+            </div>
+          </div>
+        </div>
+        {/* <div className='exit-game-btn-div center'>
                     <button className='exit-game-btn center'>
                         <span>Exit Game</span> 
                         <i class="fa fa-sign-out" aria-hidden="true"></i>
                         </button>
                 </div> */}
-                <div className='exit-game-btn-div center'>
-                    <button className='exit-game-btn center'>
-                        <span>Exit Game</span> 
-                        <i class="fa fa-sign-out" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </div>
-         
+        <div className='exit-game-btn-div center'>
+          <button className='exit-game-btn center'>
+            <span>Exit Game</span>
+            <i class='fa fa-sign-out' aria-hidden='true'></i>
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Room
+export default Room;
